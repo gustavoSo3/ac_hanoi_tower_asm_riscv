55d379856f9a8d0cc0a7bda03bf5e09f1df8b84b
Gustavo Sotres <gustavonline.games@gmail.com>
2022-03-02 11:54:30 -0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,8 +2,12 @@
 .data
 .text
 
-main:
 
-hanoid_recursive:
+prep: # We will use this to set the n of disks and fill the disk on order from n to 1 in the space thery must be
 
-end:
+
+main: # Here well call the first time the hanoid_recursive section and jump to the end of the program
+
+hanoid_recursive: # This is the section that will be call recursivelly 
+
+end: # This is just the en of the program.
